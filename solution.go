6c7c61c2729c385126cd0c078d37172090ad987e
sunquake <67069382+sunquake@users.noreply.github.com>
2022-02-11 22:37:54 +0600

    Update solution.go

--- solution.go
+++ solution.go
@@ -1,7 +1,7 @@
 package solution
 
-import "github.com/kyokomi/emoji/v2"
+import . "github.com/kyokomi/emoji/v2"
 
 func GetMessage() string {
-	return emoji.Sprint("Hello :world_map:!")
+	return Sprint("Hello :world_map:!")
 }
